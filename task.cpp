3a42bfb459aee22dcf431af2c6ac2a64855217d5
joseph-cruz <joseph.cruz@mail.utoronto.ca>
2018-08-31 09:39:13 -0400

    STUN request for public address/port; WIP ICE request for domain server address/port

--- task.cpp
+++ task.cpp
@@ -5,9 +5,118 @@ Task::Task(QObject * parent) :
     client_address(QHostAddress::LocalHost),
     client_port(4444),
     server_address(QHostAddress::LocalHost),
-    server_port(5555)
+    server_port(5555),
+    stun_server_hostname("stun.highfidelity.io"),
+    stun_server_port(3478),
+    ice_server_hostname("ice.highfidelity.com"), //"dev-ice.highfidelity.com";
+    ice_server_port(7337)
 {
-    qDebug() << "Task::Task()";
+    hasCompletedInitialStun = false;
+    numInitialStunRequests = 0;
+    hasCompletedInitialIce = false;
+    numInitialIceRequests = 0;
+    hasTCPCheckedLocalSocket = false;
+    iceClientID = QUuid::createUuid();
+
+    networkAccessManager = new QNetworkAccessManager();
+
+    qDebug() << "Task::Task() - Synchronously looking up IP address for hostname" << stun_server_hostname;
+    QHostInfo result_stun = QHostInfo::fromName(stun_server_hostname);
+    handleLookupResult(result_stun, &stun_server_address);
+    qDebug() << "Task::Task() - STUN server IP address: " << stun_server_address;
+
+    qDebug() << "Task::Task() - Synchronously looking up IP address for hostname" << ice_server_hostname;
+    QHostInfo result_ice = QHostInfo::fromName(ice_server_hostname);
+    handleLookupResult(result_ice, &ice_server_address);
+    qDebug() << "Task::Task() - ICE server IP address: " << ice_server_address;
+
+    updateLocalSocket();
+}
+
+void Task::updateLocalSocket()
+{
+    // attempt to use Google's DNS to confirm that local IP
+    static const QHostAddress RELIABLE_LOCAL_IP_CHECK_HOST = QHostAddress { "8.8.8.8" };
+    static const int RELIABLE_LOCAL_IP_CHECK_PORT = 53;
+
+    QTcpSocket* localIPTestSocket = new QTcpSocket;
+
+    connect(localIPTestSocket, &QTcpSocket::connected, this, &Task::connectedForLocalSocketTest);
+    connect(localIPTestSocket, SIGNAL(error(QAbstractSocket::SocketError)), this, SLOT(errorTestingLocalSocket()));
+
+    // attempt to connect to our reliable host
+    localIPTestSocket->connectToHost(RELIABLE_LOCAL_IP_CHECK_HOST, RELIABLE_LOCAL_IP_CHECK_PORT);
+}
+
+void Task::connectedForLocalSocketTest()
+{
+    auto localIPTestSocket = qobject_cast<QTcpSocket*>(sender());
+
+    if (localIPTestSocket) {
+        auto localHostAddress = localIPTestSocket->localAddress();
+
+        if (localHostAddress.protocol() == QAbstractSocket::IPv4Protocol) {
+            local_address = localHostAddress;
+
+            qDebug() << "Task::connectedForLocalSocketTest() - Local address: " << local_address;
+
+            hasTCPCheckedLocalSocket = true;
+        }
+
+        localIPTestSocket->deleteLater();
+    }
+}
+
+void Task::errorTestingLocalSocket()
+{
+    auto localIPTestSocket = qobject_cast<QTcpSocket*>(sender());
+
+    if (localIPTestSocket) {
+
+        // error connecting to the test socket - if we've never set our local socket using this test socket
+        // then use our possibly updated guessed local address as fallback
+        if (!hasTCPCheckedLocalSocket) {
+            local_address = getGuessedLocalAddress();
+
+            qDebug() << "Task::errorTestingLocalSocket() - Local address: " << local_address;
+
+            hasTCPCheckedLocalSocket = true;
+        }
+
+        localIPTestSocket->deleteLater();;
+    }
+}
+
+
+QHostAddress Task::getGuessedLocalAddress()
+{
+    QHostAddress localAddress;
+
+    foreach(const QNetworkInterface &networkInterface, QNetworkInterface::allInterfaces()) {
+        if (networkInterface.flags() & QNetworkInterface::IsUp
+            && networkInterface.flags() & QNetworkInterface::IsRunning
+            && networkInterface.flags() & ~QNetworkInterface::IsLoopBack) {
+            // we've decided that this is the active NIC
+            // enumerate it's addresses to grab the IPv4 address
+            foreach(const QNetworkAddressEntry &entry, networkInterface.addressEntries()) {
+                // make sure it's an IPv4 address that isn't the loopback
+                if (entry.ip().protocol() == QAbstractSocket::IPv4Protocol && !entry.ip().isLoopback()) {
+
+                    // set our localAddress and break out
+                    localAddress = entry.ip();
+                    qDebug() << "Task::getGuessedLocalAddress() - " << localAddress;
+                    break;
+                }
+            }
+        }
+
+        if (!localAddress.isNull()) {
+            break;
+        }
+    }
+
+    // return the looked up local address
+    return localAddress;
 }
 
 void Task::ProcessCommandLineArguments(int argc, char * argv[])
@@ -24,6 +133,10 @@ void Task::ProcessCommandLineArguments(int argc, char * argv[])
             client_port = QString(argv[i+2]).toInt();
             i+=2;
         }
+        else if (s.right(7) == "-domain" && i+1 < argc) {
+            domain_id = QUuid(QString(argv[i+1]));
+            i+=2;
+        }
         else if (s.right(5) == "-help") {
             qDebug() << "Usage: \n hifi_webrtc_relay [-client address port] [-server address port] [-help]";
 
@@ -36,7 +149,7 @@ void Task::ProcessCommandLineArguments(int argc, char * argv[])
 void Task::run()
 {
     // Do processing here
-    qDebug() << "Task::run()";
+    //qDebug() << "Task::run()";
 
     //setup client socket for receiving
     client_socket = new QUdpSocket(this);
@@ -55,11 +168,52 @@ void Task::run()
 
     connect(signal_mapper, SIGNAL(mapped(QString)), this, SLOT(readPendingDatagrams(QString)));
 
-    //QString test = "test";
-    //client_socket->writeDatagram(test.toLatin1(), client_address, client_port);
+    ice_stun_socket = new QUdpSocket(this);
+    connect(ice_stun_socket, SIGNAL(readyRead()), this, SLOT(parseStunResponse()));
+
+    stunResponseTimer = new QTimer { this };
+    const int STUN_INITIAL_UPDATE_INTERVAL_MSECS = 250;
+    connect(stunResponseTimer, &QTimer::timeout, this, &Task::sendStunRequest);
+    stunResponseTimer->setInterval(STUN_INITIAL_UPDATE_INTERVAL_MSECS); // 250ms, Qt::CoarseTimer acceptable
+
+    //qDebug() << "Domain ID" << domain_id;
+
+    connect(this, SIGNAL(stunFinished()), this, SLOT(startIce()));
+
+    stunResponseTimer->start();
 
     // Application runs indefinitely (until terminated - e.g. Ctrl+C)
     //    emit finished();
+}
+
+void Task::startIce()
+{
+    disconnect(ice_stun_socket, SIGNAL(readyRead()), this, SLOT(parseStunResponse()));
+    connect(ice_stun_socket, SIGNAL(readyRead()), this, SLOT(parseIceResponse()));
+
+    iceResponseTimer = new QTimer { this };
+    const int ICE_INITIAL_UPDATE_INTERVAL_MSECS = 250;
+    connect(iceResponseTimer, &QTimer::timeout, this, &Task::sendIceRequest);
+    iceResponseTimer->setInterval(ICE_INITIAL_UPDATE_INTERVAL_MSECS); // 250ms, Qt::CoarseTimer acceptable
+
+    iceResponseTimer->start();
+}
+
+void Task::handleLookupResult(const QHostInfo& hostInfo, QHostAddress * addr)
+{
+    if (hostInfo.error() != QHostInfo::NoError) {
+        qDebug() << "Task::handleLookupResult() - Lookup failed for" << hostInfo.lookupId() << ":" << hostInfo.errorString();
+    } else {
+        foreach(const QHostAddress& address, hostInfo.addresses()) {
+            // just take the first IPv4 address
+            if (address.protocol() == QAbstractSocket::IPv4Protocol) {
+                *addr = QHostAddress(address);
+                qDebug() << "Task::handleLookupResult() - QHostInfo lookup result for"
+                    << hostInfo.hostName() << "with lookup ID" << hostInfo.lookupId() << "is" << address.toString();
+                break;
+            }
+        }
+    }
 }
 
 void Task::readPendingDatagrams(QString f)
@@ -81,8 +235,227 @@ void Task::readPendingDatagrams(QString f)
         from->readDatagram(datagram.data(), datagram.size(), &sender, &senderPort);
 
         //Output debug information (for debug builds, not for production release)
-        qDebug() << " read packet from " << sender << f << ":" << senderPort << " of size " << datagram.size() << " bytes";
+        qDebug() << "Task::readPendingDatagrams() - read packet from " << sender << f << ":" << senderPort << " of size " << datagram.size() << " bytes";
 
         to->writeDatagram(datagram, address, port);
     }
+}
+
+void Task::parseStunResponse()
+{
+    //qDebug() << "Task::parseStunResponse()";
+
+    // check the cookie to make sure this is actually a STUN response
+    // and read the first attribute and make sure it is a XOR_MAPPED_ADDRESS
+    const int NUM_BYTES_MESSAGE_TYPE_AND_LENGTH = 4;
+    const uint16_t XOR_MAPPED_ADDRESS_TYPE = htons(0x0020);
+
+    const uint32_t RFC_5389_MAGIC_COOKIE_NETWORK_ORDER = htonl(RFC_5389_MAGIC_COOKIE);
+
+    int attributeStartIndex = NUM_BYTES_STUN_HEADER;
+
+    while (ice_stun_socket->hasPendingDatagrams()) {
+        QByteArray datagram;
+        datagram.resize(ice_stun_socket->pendingDatagramSize());
+        QHostAddress sender;
+        quint16 senderPort;
+
+        ice_stun_socket->readDatagram(datagram.data(), datagram.size(), &sender, &senderPort);
+
+        if (memcmp(datagram.data() + NUM_BYTES_MESSAGE_TYPE_AND_LENGTH,
+                   &RFC_5389_MAGIC_COOKIE_NETWORK_ORDER,
+                   sizeof(RFC_5389_MAGIC_COOKIE_NETWORK_ORDER)) != 0) {
+            qDebug() << "Task::parseStunResponse() - STUN response cannot be parsed, magic cookie is invalid";
+            return;
+        }
+
+        // enumerate the attributes to find XOR_MAPPED_ADDRESS_TYPE
+        while (attributeStartIndex < datagram.size()) {
+            if (memcmp(datagram.data() + attributeStartIndex, &XOR_MAPPED_ADDRESS_TYPE, sizeof(XOR_MAPPED_ADDRESS_TYPE)) == 0) {
+                const int NUM_BYTES_STUN_ATTR_TYPE_AND_LENGTH = 4;
+                const int NUM_BYTES_FAMILY_ALIGN = 1;
+                const uint8_t IPV4_FAMILY_NETWORK_ORDER = htons(0x01) >> 8;
+
+                int byteIndex = attributeStartIndex + NUM_BYTES_STUN_ATTR_TYPE_AND_LENGTH + NUM_BYTES_FAMILY_ALIGN;
+
+                uint8_t addressFamily = 0;
+                memcpy(&addressFamily, datagram.data() + byteIndex, sizeof(addressFamily));
+
+                byteIndex += sizeof(addressFamily);
+
+                if (addressFamily == IPV4_FAMILY_NETWORK_ORDER) {
+                    // grab the X-Port
+                    uint16_t xorMappedPort = 0;
+                    memcpy(&xorMappedPort, datagram.data() + byteIndex, sizeof(xorMappedPort));
+
+                    public_port = ntohs(xorMappedPort) ^ (ntohl(RFC_5389_MAGIC_COOKIE_NETWORK_ORDER) >> 16);
+
+                    byteIndex += sizeof(xorMappedPort);
+
+                    // grab the X-Address
+                    uint32_t xorMappedAddress = 0;
+                    memcpy(&xorMappedAddress, datagram.data() + byteIndex, sizeof(xorMappedAddress));
+
+                    uint32_t stunAddress = ntohl(xorMappedAddress) ^ ntohl(RFC_5389_MAGIC_COOKIE_NETWORK_ORDER);
+
+                    // QHostAddress newPublicAddress(stunAddress);
+                    public_address = QHostAddress(stunAddress);
+
+                    qDebug() << "Task::parseStunResponse() - Public address: " << public_address;
+                    qDebug() << "Task::parseStunResponse() - Public port: " << public_port;
+
+                    hasCompletedInitialStun = true;
+
+                    emit stunFinished();
+
+                    return;
+                }
+            } else {
+                // push forward attributeStartIndex by the length of this attribute
+                const int NUM_BYTES_ATTRIBUTE_TYPE = 2;
+
+                uint16_t attributeLength = 0;
+                memcpy(&attributeLength, datagram.data() + attributeStartIndex + NUM_BYTES_ATTRIBUTE_TYPE,
+                       sizeof(attributeLength));
+                attributeLength = ntohs(attributeLength);
+
+                attributeStartIndex += NUM_BYTES_MESSAGE_TYPE_AND_LENGTH + attributeLength;
+            }
+        }
+    }
+}
+
+void Task::parseIceResponse()
+{
+    qDebug() << "ICE RESPONSE";
+    hasCompletedInitialIce = true;
+}
+
+void Task::sendStunRequest()
+{
+    const int NUM_INITIAL_STUN_REQUESTS_BEFORE_FAIL = 10;
+
+    if (hasTCPCheckedLocalSocket && numInitialStunRequests == 0)
+    {
+        ice_stun_socket->bind(local_address);
+    }
+    else
+    {
+        return;
+    }
+
+    if (numInitialStunRequests == NUM_INITIAL_STUN_REQUESTS_BEFORE_FAIL)
+    {
+        qDebug() << "Task::sendStunRequest() - Stopping stun requests to" << stun_server_hostname << stun_server_address << stun_server_port;
+        stunResponseTimer->stop();
+        stunResponseTimer->deleteLater();
+        return;
+    }
+
+    if (!hasCompletedInitialStun) {
+        qDebug() << "Task::sendStunRequest() - Sending intial stun request to" << stun_server_hostname << stun_server_address << stun_server_port;
+        ++numInitialStunRequests;
+    }
+    else {
+        qDebug() << "Task::sendStunRequest() - Completed stun request";
+        stunResponseTimer->stop();
+        stunResponseTimer->deleteLater();
+        return;
+    }
+
+    char * stunRequestPacket = (char *) malloc(NUM_BYTES_STUN_HEADER);
+    makeStunRequestPacket(stunRequestPacket);
+    ice_stun_socket->writeDatagram(stunRequestPacket, NUM_BYTES_STUN_HEADER, stun_server_address, stun_server_port);
+}
+
+void Task::sendIceRequest()
+{
+    const int NUM_INITIAL_ICE_REQUESTS_BEFORE_FAIL = 10;
+
+    if (!hasTCPCheckedLocalSocket) {
+        return;
+    }
+
+    if (numInitialIceRequests == NUM_INITIAL_ICE_REQUESTS_BEFORE_FAIL)
+    {
+        qDebug() << "Task::sendIceRequest() - Stopping ice requests to" << ice_server_hostname << ice_server_address << ice_server_port;
+        iceResponseTimer->stop();
+        iceResponseTimer->deleteLater();
+        return;
+    }
+
+    if (!hasCompletedInitialIce) {
+        qDebug() << "Task::sendIceRequest() - Sending intial ice request to" << ice_server_hostname << ice_server_address << ice_server_port;
+        ++numInitialIceRequests;
+    }
+    else {
+        qDebug() << "Task::sendIceRequest() - Completed ice request";
+        iceResponseTimer->stop();
+        iceResponseTimer->deleteLater();
+        return;
+    }
+
+    int packetSize = sizeof(uint8_t) + sizeof(char) + sizeof(iceClientID) + sizeof(public_address) + sizeof(public_port) + sizeof(local_address) + sizeof(public_port) + sizeof(domain_id);
+    char * iceRequestPacket = (char *) malloc(packetSize);
+    makeIceRequestPacket(iceRequestPacket);
+    ice_stun_socket->writeDatagram(iceRequestPacket, packetSize, ice_server_address, ice_server_port);
+}
+
+void Task::makeStunRequestPacket(char * stunRequestPacket)
+{
+    int packetIndex = 0;
+
+    const uint32_t RFC_5389_MAGIC_COOKIE_NETWORK_ORDER = htonl(RFC_5389_MAGIC_COOKIE);
+
+    // leading zeros + message type
+    const uint16_t REQUEST_MESSAGE_TYPE = htons(0x0001);
+    memcpy(stunRequestPacket + packetIndex, &REQUEST_MESSAGE_TYPE, sizeof(REQUEST_MESSAGE_TYPE));
+    packetIndex += sizeof(REQUEST_MESSAGE_TYPE);
+
+    // message length (no additional attributes are included)
+    uint16_t messageLength = 0;
+    memcpy(stunRequestPacket + packetIndex, &messageLength, sizeof(messageLength));
+    packetIndex += sizeof(messageLength);
+
+    memcpy(stunRequestPacket + packetIndex, &RFC_5389_MAGIC_COOKIE_NETWORK_ORDER, sizeof(RFC_5389_MAGIC_COOKIE_NETWORK_ORDER));
+    packetIndex += sizeof(RFC_5389_MAGIC_COOKIE_NETWORK_ORDER);
+
+    // transaction ID (random 12-byte unsigned integer)
+    const uint NUM_TRANSACTION_ID_BYTES = 12;
+    QUuid randomUUID = QUuid::createUuid();
+    memcpy(stunRequestPacket + packetIndex, randomUUID.toRfc4122().data(), NUM_TRANSACTION_ID_BYTES);
+}
+
+void Task::makeIceRequestPacket(char * iceRequestPacket)
+{
+    uint8_t packetType = 23;
+    char version = 17;
+
+    int packetIndex = 0;
+
+    memcpy(iceRequestPacket + packetIndex, &packetType, sizeof(packetType));
+    packetIndex += sizeof(packetType);
+
+    memcpy(iceRequestPacket + packetIndex, &version, sizeof(version));
+    packetIndex += sizeof(version);
+
+    memcpy(iceRequestPacket + packetIndex, &iceClientID, sizeof(iceClientID));
+    packetIndex += sizeof(iceClientID);
+
+    memcpy(iceRequestPacket + packetIndex, &public_address, sizeof(public_address));
+    packetIndex += sizeof(public_address);
+
+    memcpy(iceRequestPacket + packetIndex, &public_port, sizeof(public_port));
+    packetIndex += sizeof(public_port);
+
+    memcpy(iceRequestPacket + packetIndex, &local_address, sizeof(local_address));
+    packetIndex += sizeof(local_address);
+
+    memcpy(iceRequestPacket + packetIndex, &public_port, sizeof(public_port));
+    packetIndex += sizeof(public_port);
+
+    memcpy(iceRequestPacket + packetIndex, &domain_id, sizeof(domain_id));
+    packetIndex += sizeof(domain_id);
+
+    qDebug() << "ICE packet values" <<  packetType << (int)version << iceClientID << public_address << public_port << local_address << public_port << domain_id;
 }
--- task.h
+++ task.h
@@ -3,9 +3,18 @@
 
 #include <QObject>
 #include <QDebug>
+#include <QHostInfo>
+#include <QtNetwork>
 #include <QString>
 #include <QSignalMapper>
-#include <QtNetwork/QUdpSocket>
+#include <QThread>
+#include <QTimer>
+#include <QUuid>
+
+const uint32_t RFC_5389_MAGIC_COOKIE = 0x2112A442;
+const int NUM_BYTES_STUN_HEADER = 20;
+
+const quint16 DEFAULT_DOMAIN_SERVER_PORT = 40102;
 
 class Task : public QObject
 {
@@ -15,17 +24,39 @@ public:
 
     Task(QObject *parent = 0);
     void ProcessCommandLineArguments(int argc, char * argv[]);
+    void handleLookupResult(const QHostInfo& hostInfo, QHostAddress * addr);
+
+    void makeStunRequestPacket(char * stunRequestPacket);
+    void makeIceRequestPacket(char * iceRequestPacket);
+
+    QHostAddress getGuessedLocalAddress();
+    void connectedForLocalSocketTest();
+    void updateLocalSocket();
 
 public slots:
 
     void run();
     void readPendingDatagrams(QString f);
+    void startIce();
+
+    void sendStunRequest();
+    void parseStunResponse();
+    void sendIceRequest();
+    void parseIceResponse();
+
+    void errorTestingLocalSocket();
 
 signals:
 
+    void stunFinished();
     void finished();
 
 private:
+
+    QString uuidStringWithoutCurlyBraces(const QUuid& uuid) {
+        QString uuidStringNoBraces = uuid.toString().mid(1, uuid.toString().length() - 2);
+        return uuidStringNoBraces;
+    }
 
     QSignalMapper * signal_mapper;
 
@@ -36,6 +67,42 @@ private:
     QUdpSocket * server_socket;
     QHostAddress server_address;
     quint16 server_port;
+
+    QUdpSocket * ice_stun_socket;
+
+    QHostAddress public_address;
+    quint16 public_port;
+    QHostAddress local_address;
+
+    QHostAddress stun_server_address;
+    QString stun_server_hostname;
+    quint16 stun_server_port;
+
+    QHostAddress ice_server_address;
+    QString ice_server_hostname;
+    quint16 ice_server_port;
+
+    QTimer * stunResponseTimer;
+    bool hasCompletedInitialStun;
+    int numInitialStunRequests;
+
+    QUuid iceClientID;
+
+    QTimer * iceResponseTimer;
+    bool hasCompletedInitialIce;
+    int numInitialIceRequests;
+
+    QNetworkAccessManager * networkAccessManager;
+
+    QByteArray sessionID;
+    QUuid domain_id;
+
+    bool hasTCPCheckedLocalSocket;
+
+    /*const int ICE_HEARBEAT_INTERVAL_MSECS = 2 * 1000;
+    const int MAX_ICE_CONNECTION_ATTEMPTS = 5;
+
+    const int UDP_PUNCH_PING_INTERVAL_MS = 25;*/
 
 };
 #endif // TASK_H
